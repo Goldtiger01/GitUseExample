b21b2c25e5966a2c6a11ccd41e8eebbb0f8539b6
Hardy, Justin E <151710@s020rmc148-11.local>
2018-11-12 12:00:40 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Hardy, Justin E on 11/12/18.
+//  Copyright © 2018 Hardy, Justin E. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
