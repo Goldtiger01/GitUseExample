0ad4ce4b409ff4c8157010afd66e04e77e9c3199
Hardy, Justin E <151710@s020rmc148-11.local>
2018-11-14 10:55:23 -0600

    Added MapForItinerary Class into a Swift File

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Hardy, Justin E on 11/14/18.
+//  Copyright © 2018 Hardy, Justin E. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary : NSObject {
+    
+}
